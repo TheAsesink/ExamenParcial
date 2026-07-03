a231ca3caa9044ab80fe1d3060e6bbcbf01f8dbd
Kevin Castro <0986255169moises@gmail.com>
2026-07-03 08:25:41 -0500

    chore: crear proyecto base Spring Boot

---
+++ src/main/java/org/exampleserverlet/examenparcial/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.exampleserverlet.examenparcial;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
